b39431e5ff049a98082f3b744d70e1cbe111e1b1
jimk100 <55141616+jimk100@users.noreply.github.com>
2020-02-21 04:27:08 -0500

    Add files via upload

---
+++ Assets/Scripts/Laser.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Laser : MonoBehaviour
+{
+    public static float rightx = 35;
+    [Header("Set Dynamically")]
+    public Text scoreGT;
+    
+    
+    void Start(){
+        GameObject scoreGO=GameObject.Find("Score");
+        scoreGT=scoreGO.GetComponent<Text>();
+        scoreGT.text=	"0";
+    }
+
+    void Update()
+    {
+        if(transform.position.x > rightx)
+        {
+            Destroy(this.gameObject);
+        }
+
+    }
+
+    void OnCollisionEnter(Collision coll)
+    {
+        GameObject collidedWith = coll.gameObject;
+        if( collidedWith.tag == "Object")
+        {
+            Destroy (collidedWith);
+            Destroy(this.gameObject);
+            
+            int score=int.Parse(scoreGT.text);
+            score+=100;
+            scoreGT.text=score.ToString();
+        
+        if	(score	>	HighScore.score)	
+        {	
+            HighScore.score	=score;				
+        } 
+        }
+        
+                //Add points for cathing the  Apple
+              
+              
+    
+}}
---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+     public Text scoreText;
+     public int score;
+ 
+     void Start() {
+         scoreText.text = "Score: " + score; // To make sure the score text is accurate
+     }
+ 
+     public void IncreaseScore() {
+         score++;
+         scoreText.text = "Score: " + score;			
+        } 
+        
+    }
+
